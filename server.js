d02085d5cdc860917072485730a2459230d50c3c
yawikflame <30662244+yawikflame@users.noreply.github.com>
2021-07-21 16:14:01 +0300

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
